fd5db5c361c65b24c34d814fb7dad8eebda15c86
kayisha Ulysse <kayishaulysse@kayishas-MBP.fios-router.home>
2023-04-29 00:29:42 -0400

    picture working, homepage layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,24 @@
 import React from "react"
+import Image from 'next/image'
+import profilePic from '../../public/images/profile/flower.png'
+
 
 const Layout = ({childern, className=""}) => {
   return (
-    <div className={`w-full h-full inliine-block z-0 bg-light p-32 ${className}`}>
-      {childern}
+    <div className={`${className}`}>
+     {childern}
+     <div className='flex items-center justify-between w-full'>
+            <div className='w-1/2'>
+              <Image src={profilePic} alt='Kayisha' className='w-full h-auto' />
+            </div>
+            <div className="w-1/2">
+                <h1> Kayisha Ulysse </h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+            </div>
+          </div>
     </div>
   )
 };
 
-export default Layout;
+export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,15 @@
 import { Inter } from 'next/font/google'
 import Layout from "@/components/Layout"
 import Image from 'next/image'
-import profilePic from "../../public/images/profile/flower.png"
+
 
 
 export default function Home() {
   return (
     <>
+     
       <main className='flex items-center text-dark w-full min-h-screen'>
-        <Layout>
-            <div className='flex items-center justify-between w-full'>
-              <div>
-                <Image src={profilePic} alt='Kayisha' className='w-full h-auto' />
-              </div>
-            </div>
-        </Layout>
+        <Layout className='pt-0 pl-10 pr-10'/>
       </main>
     </>
   )
